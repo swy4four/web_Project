86b8a4552644f380dc63f98aa8e876cada467006
Song <worktomyself@outlook.com>
2024-05-16 13:13:10 +0800

    2024.5.16提交第一次

---
+++ web_js/youtube_Js/index.js
@@ -0,0 +1,27 @@
+// console.log('hello,world');
+
+/* let name ='song';
+console.log(name);  */
+
+/* const interestRate=0.3;
+let interestRate=1;
+console.log(interestRate); */
+
+// 数据类型
+/* let name ='song'; //String
+let age =21;    //Number 
+let isApproved=false; // boolean
+let firstName=undefined; 
+let selectColor=null;
+ */
+
+/* let selectedColors =['red','blue'];
+selectedColors[2]='green'; 
+selectedColors[3]=3;
+console.log(selectedColors); */
+
+function greet(name){
+    console.log('hello,world'+name);
+}
+
+greet('song');
