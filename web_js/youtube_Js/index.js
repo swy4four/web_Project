63add0a3db054223189c35bc3dadcf87abea5494
Song <worktomyself@outlook.com>
2024-05-17 15:58:13 +0800

    2024.5.17 js常用对象

--- web_js/youtube_Js/index.js
+++ web_js/youtube_Js/index.js
@@ -20,8 +20,61 @@ selectedColors[2]='green';
 selectedColors[3]=3;
 console.log(selectedColors); */
 
-function greet(name){
+/* function greet(name){
     console.log('hello,world'+name);
 }
 
-greet('song');
+greet('song'); */
+
+/* console.log(
+    '%cThis text is styled!',
+    'color: red; background: yellow; font-size: 24px;'
+  )  */
+
+  /* var languages = [
+    { name: "JavaScript", fileExtension: ".js" },
+    { name: "TypeScript", fileExtension: ".ts" },
+    { name: "CoffeeScript", fileExtension: ".coffee" }
+  ];
+  
+  console.table(languages);
+
+  var languages = {
+    csharp: { name: "C#", paradigm: "object-oriented" },
+    fsharp: { name: "F#", paradigm: "functional" }
+  };
+  
+  console.table(languages); */
+
+/*   function greet(user) {
+    console.count();
+    return 'hi ' + user;
+  }
+  
+  greet('bob')
+  //  : 1
+  // "hi bob"
+  
+  greet('alice')
+  //  : 2
+  // "hi alice"
+  
+  greet('bob')
+  //  : 3
+  // "hi bob" */
+
+
+  //构造函数 
+var Vehicle =function(name,model){
+    this.name=name;
+    this.model=model;
+
+    this.comment=function (){
+        console.log("是一辆车")
+    }
+}
+
+var car1=new Vehicle('bigcar1','big');
+car1.comment();
+console.log(car1);
+
